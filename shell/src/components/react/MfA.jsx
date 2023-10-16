553f10b9f6c89f2d3f450b147f712bae8e9f93ea
Raymond Boswel <raymondboswel@gmail.com>
2023-10-16 17:36:55 +0200

    WIP - event handlers on side nav goes missing.

--- shell/src/components/react/MfA.jsx
+++ shell/src/components/react/MfA.jsx
@@ -2,11 +2,11 @@
 
 import React from "react";
 import MicroFrontendA from "http://localhost:8080/bundle-a.js";
-import DevFrontendA from "http://localhost:7100/bundle.js";
+// import DevFrontendA from "http://localhost:7100/bundle.js";
 const MfA = () => {
   const isDev = import.meta.env.DEV;
   if (isDev) {
-    return <DevFrontendA />;
+    return <MicroFrontendA/>;
   } else {
     return <MicroFrontendA />;
   }
